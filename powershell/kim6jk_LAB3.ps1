30f1ee2d4f4f70ea13598008483d92933d7e66f7
kim6jk <kim6jk@mail.uc.edu>
2022-09-18 11:08:44 -0400

    powershell files

---
+++ powershell/kim6jk_LAB3.ps1
@@ -0,0 +1,9 @@
+﻿$IP = (Get-NetIPAddress).IPv4Address | Select-String "192*"
+$user = $env:USERNAME
+$hostname = $env:COMPUTERNAME
+$version = $Host.Version.Major
+$date = Get-Date -Format "dddd, MM dd yyyy"
+
+$body = "This machine's IP is {0}. User is {1}. Hostname is {2}. PowerShell Version {3}. Today's Date is {4}." -f $IP, $user, $hostname, $version, $date
+
+$body | Out-File -Append C:\Users\Administrator\Desktop\sysinfo.txt
