a42b3288fd746794ff811c189e8c9e6e23e25ad5
kim6jk <112104258+kim6jk@users.noreply.github.com>
2022-10-29 16:44:27 -0400

    Update project2.py

--- Project2/project2.py
+++ Project2/project2.py
@@ -8,7 +8,8 @@ y = []
 # Change the title of the matplotlib window
 plt.title('Project 2!')
 
-print('Use the file picker to load a .txt file with x,y value pair. Like the one provided on Github')
+print('Use the file picker to load a .txt file with x,y value pair. Like the file provided on Github')
+# I learned about this method from https://stackoverflow.com/questions/3579568/choosing-a-file-in-python-with-simple-dialog
 filename = askopenfilename()
 
 print('What graph would you like?\n1) Plot\n2) Scatter\n3) Bar\n4) Step')
@@ -19,16 +20,16 @@ if (graph == 1):
         ls = int(input())
 
 
-# Open the selected file in 'r' (Read) mode
+# Open the selected file in 'r' (Read) mode from https://www.pythonprogramming.net/loading-file-data-matplotlib-tutorial/
 with open(filename,'r') as file:
         points = csv.reader(file, delimiter = ',')
-        # This sorts the list of points by the X value
+        # This sorts the list of points by the X value // learned about lambda sorting here https://pythonsolved.com/how-to-sort-2d-array-in-python/
         points = sorted(points, key = lambda i: int(i[0]))
         for value in points:
             x.append(int(value[0]))
             y.append(int(value[1]))
 
-
+# Learned about matplotlib from their website specifically https://matplotlib.org/cheatsheets/_images/cheatsheets-1.png
 if(graph == 1):
         if(ls == 1):
                 plt.plot(x,y,'-')
