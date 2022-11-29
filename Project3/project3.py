33a3341c74af2c72af44726d39b48df917985e5f
kim6jk <112104258+kim6jk@users.noreply.github.com>
2022-11-29 17:41:56 -0500

    Update project3.py

--- Project3/project3.py
+++ Project3/project3.py
@@ -9,13 +9,13 @@ import csv
 def getRadioButtonValue():
 	buttonSelected = rbType.get()
 	return buttonSelected
-
+# funciton to disable/enable the OptionMenu when plot is selected 
 def changeState():
     if (getRadioButtonValue() == 1):
         drop.configure(state="normal")
     else:
         drop.configure(state="disabled")
-
+# function to handle selecting a file, then parsing the file and ordering the plot points ascending and add it to and x & y array
 def selectFile():
     global filename
     filename = askopenfilename()
@@ -30,11 +30,11 @@ def selectFile():
         for value in points:
             x.append(int(value[0]))
             y.append(int(value[1]))
-
+# function to change a global variable to get line style from the OptionMenu to the graphIt function
 def setStyle(lineStyle):
     global ls
     ls = lineStyle
-
+# function with checks to create the correct graph based on user choices and then plot it
 def graphIt():
     graph = getRadioButtonValue()
     if(graph == 1):
@@ -54,18 +54,19 @@ def graphIt():
         plt.step(x,y)
     plt.show()
 
-
+# create the root window
 root = Tk()
 
 rbType = tk.IntVar()
 rbLine = tk.StringVar()
 ls = "Solid"
 
+# set the size of the gui and set the background color and title
 root.geometry('300x300')
 root.configure(background='#FAEBD7')
 root.title('Project 3')
 
-
+# Create a set of radio buttons for graph type selection
 frame=Frame(root, width=0, height=0, bg='#FAEBD7')
 frame.place(x=37, y=28)
 ARBEES=[
@@ -77,7 +78,7 @@ ARBEES=[
 for text, mode in ARBEES:
 	rbGroupOne=Radiobutton(frame, text=text, variable=rbType, value=mode, bg='#FAEBD7', font=('arial', 12, 'normal'),command=changeState)
 	rbGroupOne.pack(side='top', anchor = 'w')
-
+# Create OptionMenu for graph style selection
 frame=Frame(root, width=0, height=0, bg='#FAEBD7')
 frame.place(x=120, y=28)
 lineStyles = [
@@ -90,12 +91,12 @@ rbLine.set("Solid")
 drop = OptionMenu(frame, rbLine, *lineStyles, command=setStyle)
 drop.configure(state="disabled")
 drop.pack()
-
+# create a button to call the selectFile function
 frame=Frame(root, width=0, height=0, bg='#FAEBD7')
 frame.place(x=120,y=75)
 filePicker = Button(frame, text="Select a File", command=selectFile)
 filePicker.pack()
-
+# create a button to call the graphIt function
 frame=Frame(root, width=0, height=0, bg='#FAEBD7')
 frame.place(x=50,y=175)
 graph = Button(frame, text="Graph it!", command=graphIt)
